bdcc060166563a3df08d3bc36f5ebbbe9f5c1b39
naruteride <tadanide@gmail.com>
2019-01-06 18:37:11 +0900

    모바일에서 비밀번호 확인 오류 정상작동하도록 고침

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -3,6 +3,10 @@ onload = function () {
     let confirmPw = document.querySelector("#confirmPassword");
     let confirmPwLabel = document.querySelector("#confirmPassword-label");
 
+    let Mpw = document.querySelector("#MRpassword");
+    let MconfirmPw = document.querySelector("#MconfirmPassword");
+    let MconfirmPwLabel = document.querySelector("#MconfirmPassword-label");
+
     confirmPw.addEventListener("focusout", e => {
         if (confirmPw.value == "") {
             confirmPw.classList.remove("valid");
@@ -38,6 +42,44 @@ onload = function () {
         } else {
             confirmPw.classList.add("valid");
             confirmPw.classList.remove("input-error");
+        }
+    })
+
+    MconfirmPw.addEventListener("focusout", e => {
+        if (MconfirmPw.value == "") {
+            MconfirmPw.classList.remove("valid");
+            MconfirmPw.classList.remove("invalid");
+        } else if (Mpw.value === MconfirmPw.value) {
+            MconfirmPw.classList.add("valid");
+            MconfirmPw.classList.remove("invalid");
+        } else {
+            MconfirmPw.classList.add("invalid");
+            MconfirmPw.classList.remove("valid");
+            MconfirmPwLabel.classList.add("label-error");
+        }
+    })
+
+    Mpw.addEventListener("focusout", e => {
+        if (MconfirmPw.value == "") {
+            MconfirmPw.classList.remove("valid");
+            MconfirmPw.classList.remove("invalid");
+        } else if (Mpw.value === MconfirmPw.value) {
+            MconfirmPw.classList.add("valid");
+            MconfirmPw.classList.remove("invalid");
+        } else {
+            MconfirmPw.classList.add("invalid");
+            MconfirmPw.classList.remove("valid");
+            MconfirmPwLabel.classList.add("label-error");
+        }
+    })
+
+    MconfirmPw.addEventListener("focus", e => {
+        if (MconfirmPw.classList.contains("invalid")) {
+            MconfirmPw.classList.add("input-error");
+            MconfirmPw.classList.remove("valid");
+        } else {
+            MconfirmPw.classList.add("valid");
+            MconfirmPw.classList.remove("input-error");
         }
     })
 };
