24487e06a0544d83b92fb56ee81e2d6b195b9b9f
naruteride <tadanide@gmail.com>
2019-01-10 16:17:26 +0900

    JS 셀렉트 생성, 쿼리셀렉터의 선택자 오타 수정 & 폰트 경로 수정 & 이메일 도메인  셀렉트화 & 팝업모델 이모티콘 지움

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -24,7 +24,7 @@ onload = function () {
     MRpw = document.querySelector("#MRpassword");
     MRconfirmPw = document.querySelector("#MconfirmPassword");
     MRconfirmPwLabel = document.querySelector("#MconfirmPassword-label");
-    MRemail = document.querySelector("#Memail");
+    MRemail = document.querySelector("#MRemail");
     MRschool = document.querySelector("#Mschool");
 
     confirmPw.addEventListener("focusout", e => {
@@ -163,4 +163,9 @@ document.addEventListener('DOMContentLoaded', function() {
 
     var MLheader = document.querySelector("#MLheader");
     var MRheader = document.querySelector("#MRheader");
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems);
 });
