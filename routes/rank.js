d32d608d72728dbf749d8442e48b5c9c45159c2e
giljong <minjong5054@daum.net>
2019-01-17 14:22:56 +0900

    하 시버ㅏㄹ

--- routes/rank.js
+++ routes/rank.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const db = require('../db/connection');
+const path = require('path');
 const router = express.Router();
 
-router.get('/rank',(req,res) => {
-    db.query('select SCORE,SCHOOL,ID,PROFILE_COMMENT from Users ORDER BY SCORE DESC', (err,result) => {
+router.get('/',(req,res) => {
+    console.log('sival')
+    db.query('select SCORE,SCHOOL,ID,PROFILE_COMMENT from Users', (err,result) => {
         if (err) throw err;
         if(!(req.session.user === undefined)){
             if(!(req.session.flag))
                 res.redirect('/auth');
             else{
                 res.render('rank.ejs',{
-                    users : result,
-                    user_id : req.session.user,
-                    user_school : req.session.school
+                    users : result
                 })
+               //res.sendFile(path.join(__dirname,'../views', 'rank.html'));
             }
         }
         else{
