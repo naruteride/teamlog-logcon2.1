e9c0738fbc2dd46a6e61cfb3a38c3256ba79709e
giljong <minjong5054@daum.net>
2019-01-18 16:30:54 +0900

    로그에 시간 찍음

--- routes/login.js
+++ routes/login.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const db = require('../db/connection');
 const crypto = require('crypto');
+const moment = require('moment');
 const router = express.Router();
 
 router.post('/',(req,res)=>{
     const id = req.body.id;
     var tmpPw = req.body.pw;
+    const time = moment().format('MMMM Do YYYY, h:mm:ss a');
     const pw = crypto.createHash('sha512').update(tmpPw).digest('base64');
     db.query('select * from Users where ID = ? and PW = ?' , [id,pw], (err, result) => {
 		if (err) throw err;
         if(result.length === 0){
             res.send('<script type="text/javascript">alert("로그인 실패!(# `)3′");window.location.href="/"</script>');
-            console.log(id + ' 로그인 실패');
+            console.log(time+': '+id + ' 로그인 실패');
         }
         else {
             req.session.flag = result[0].FLAG;
@@ -19,7 +21,7 @@ router.post('/',(req,res)=>{
             req.session.score = result[0].SCORE;
             req.session.school = result[0].SCHOOL;
             req.session.save(() => {
-                console.log(id + ' 로그인 성공');
+                console.log(time+ ': '+id + ' 로그인 성공');
                 res.send('<script type="text/javascript">alert("로그인 성공!(｡◝‿◜｡)");window.location.href = "/";</script>');
 			})
         }  
