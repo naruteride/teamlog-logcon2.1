0b8c03bf31edf472d04d8584ef3e5165f530aad7
giljong <minjong5054@daum.net>
2019-01-19 11:44:15 +0900

    슈ㅏ아벌

--- routes/challenge.js
+++ routes/challenge.js
@@ -8,14 +8,14 @@ router.get('/:num',(req,res) => {
         if(!(req.session.flag))
             res.redirect('/auth');
         else{
-            pnum = req.params.num;
-            db.query('select CONTENTS,TITLE,SCORE,FILE_PATH from Problems where ID = ?',pnum,(err,result) => {
+            req.session.num = req.params.num;
+            db.query('select CONTENTS,TITLE,SCORE,FILE_PATH from Problems where ID = ?',req.session.num,(err,result) => {
                 if(err) throw err;
                 res.render('challenge.ejs',{
                     info : result,
                     user_id : req.session.user,
                     user_school: req.session.school,
-                    pid : pnum,
+                    pid : req.session.num,
                     score : req.session.score
                  })
             })
@@ -32,15 +32,15 @@ router.post('/:num',(req,res) => {
     req.connection.remoteAddress ||
     req.socket.remoteAddress ||
     req.connection.socket.remoteAddress;
-    pid = req.params.num;
-    db.query('select ANSWER,SCORE from Problems where ID = ?',pid,(err,result) => {
+    pid = req.session.num;
+    db.query('select ANSWER,SCORE from Problems where ID = ?',req.session.num,(err,result) => {
         if(err) throw err;
         if(result[0].ANSWER === ans){
-            db.query('select * from Solved where PID = ? and USER = ?',[pid,user],(error,results) => {
+            db.query('select * from Solved where PID = ? and USER = ?',[req.session.num,user],(error,results) => {
                 if(error) console.log(error);
                 if(results.length === 0){
                     db.query('update Users set SCORE=? where ID = ?',[result[0].SCORE + req.session.score,user]);
-                    db.query('insert into Solved (PID,USER) values (?,?)',[pid,user]);
+                    db.query('insert into Solved (PID,USER) values (?,?)',[req.session.num,user]);
                     req.session.score += result[0].SCORE;
                     req.session.save(() => {
                         res.send('<script type="text/javascript">alert("정답!!!٩(๑❛ワ❛๑)و");window.location.href="/challenges";</script>');
